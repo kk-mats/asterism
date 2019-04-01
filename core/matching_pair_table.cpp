5314ea3c8338470f11dfbcaed6d983c08fc69277
kk-mats <k.mats.kk@gmail.com>
2019-04-02 02:02:47 +0900

    add the jcln reference file

---
+++ core/matching_pair_table.cpp
@@ -0,0 +1,6 @@
+#include "equivalent_pair_table.hpp"
+
+equivalent_pair_table::equivalent_pair_table()
+{
+
+}
---
+++ core/matching_pair_table.hpp
@@ -0,0 +1,11 @@
+#ifndef EQUIVALENT_PAIR_TABLE_HPP
+#define EQUIVALENT_PAIR_TABLE_HPP
+
+
+class equivalent_pair_table
+{
+public:
+	equivalent_pair_table();
+};
+
+#endif // EQUIVALENT_PAIR_TABLE_HPP
