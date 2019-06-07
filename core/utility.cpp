7a868450d5d2a7a6586bedd80fe2e8c91e1b56d6
mat <k.mats.kk@gmail.com>
2019-06-07 20:46:33 +0900

    use same separators for file class

--- core/utility.cpp
+++ core/utility.cpp
@@ -5,12 +5,12 @@ namespace asterism
 
 QString to_canonical_file_path(const QString &path) noexcept
 {
-	return QFileInfo(path).canonicalFilePath();
+	return QFileInfo(QDir::fromNativeSeparators(path)).canonicalFilePath();
 }
 
 QString to_canonical_path(const QString &path) noexcept
 {
-	return QFileInfo(path).canonicalPath();
+	return QFileInfo(QDir::fromNativeSeparators(path)).canonicalPath();
 }
 
 }
--- core/utility.hpp
+++ core/utility.hpp
@@ -1,6 +1,7 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
 
+#include <QDir>
 #include <QFileInfo>
 
 
